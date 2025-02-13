8fdc04b2fb7684d9ec9f97c61c99166b6b523bde
Danny Gershman <danny.gershman@gmail.com>
2025-02-12 21:45:35 -0500

    adding additional columns

--- includes/Admin.php
+++ includes/Admin.php
@@ -7,6 +7,10 @@ class Admin {
         add_action('init', [__CLASS__, 'register_post_type']);
         add_action('admin_menu', [__CLASS__, 'add_menu']);
         add_action('admin_enqueue_scripts', [__CLASS__, 'enqueue_scripts']);
+        
+        // Add custom columns
+        add_filter('manage_mayo_event_posts_columns', [__CLASS__, 'set_custom_columns']);
+        add_action('manage_mayo_event_posts_custom_column', [__CLASS__, 'render_custom_columns'], 10, 2);
     }
 
     public static function register_post_type() {
@@ -56,5 +60,36 @@ class Admin {
             '1.0',
             true
         );
+    }
+
+    public static function set_custom_columns($columns) {
+        return [
+            'cb' => $columns['cb'],
+            'title' => __('Event Name'),
+            'event_type' => __('Type'),
+            'event_date' => __('Date'),
+            'event_time' => __('Time'),
+            'status' => __('Status'),
+            'date' => $columns['date']
+        ];
+    }
+
+    public static function render_custom_columns($column, $post_id) {
+        switch ($column) {
+            case 'event_type':
+                echo get_post_meta($post_id, 'event_type', true);
+                break;
+            case 'event_date':
+                echo get_post_meta($post_id, 'event_date', true);
+                break;
+            case 'event_time':
+                $start = get_post_meta($post_id, 'event_start_time', true);
+                $end = get_post_meta($post_id, 'event_end_time', true);
+                echo "$start - $end";
+                break;
+            case 'status':
+                echo get_post_status($post_id);
+                break;
+        }
     }
 }
