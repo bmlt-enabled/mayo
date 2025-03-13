9a960ba55590e3df8b34510f1d51d19f03658fa2
Danny Gershman <danny.gershman@gmail.com>
2025-03-12 21:33:27 -0400

    fixing names to comply with linting

--- mayo-events-manager.php
+++ mayo-events-manager.php
@@ -43,18 +43,18 @@ add_action(
     }
 );
 
-register_activation_hook(__FILE__, 'bmltenabled_mayo_activate');
-register_deactivation_hook(__FILE__, 'bmltenabled_mayo_deactivate');
-add_action('plugins_loaded', 'bmltenabled_mayo_checkUpgrade');
-add_filter('archive_template', 'bmltenabled_mayo_loadArchiveTemplate');
-add_filter('single_template', 'bmltenabled_mayo_loadDetailsTemplate');
+register_activation_hook(__FILE__, 'Bmltenabled_Mayo_activate');
+register_deactivation_hook(__FILE__, 'Bmltenabled_Mayo_deactivate');
+add_action('plugins_loaded', 'Bmltenabled_Mayo_checkUpgrade');
+add_filter('archive_template', 'Bmltenabled_Mayo_loadArchiveTemplate');
+add_filter('single_template', 'Bmltenabled_Mayo_loadDetailsTemplate');
 
 /**
  * Activate the plugin and flush rewrite rules.
  *
  * @return void
  */
-function bmltenabled_mayo_activate()
+function Bmltenabled_Mayo_activate()
 {
     // If init hasn't fired, add an action to register post type
     if (!did_action('init')) {
@@ -73,7 +73,7 @@ function bmltenabled_mayo_activate()
  *
  * @return void
  */
-function bmltenabled_mayo_deactivate()
+function Bmltenabled_Mayo_deactivate()
 {
     // Unregister the post type
     unregister_post_type('mayo_event');
@@ -87,13 +87,13 @@ function bmltenabled_mayo_deactivate()
  *
  * @return void
  */
-function bmltenabled_mayo_checkUpgrade()
+function Bmltenabled_Mayo_checkUpgrade()
 {
     $current_version = get_option('mayo_version');
     if (empty($current_version) 
         || version_compare($current_version, MAYO_VERSION, '<')
     ) {
-        bmltenabled_mayo_activate();
+        Bmltenabled_Mayo_activate();
         update_option('mayo_version', MAYO_VERSION);
     }
 }
@@ -106,7 +106,7 @@ function bmltenabled_mayo_checkUpgrade()
  * @return string The path to the custom template if it exists,
  * otherwise the original template.
  */
-function bmltenabled_mayo_loadArchiveTemplate($template)
+function Bmltenabled_Mayo_loadArchiveTemplate($template)
 {
     if (is_post_type_archive('mayo_event')) {
         $custom_template = plugin_dir_path(__FILE__) .
@@ -126,7 +126,7 @@ function bmltenabled_mayo_loadArchiveTemplate($template)
  * @return string The path to the custom template if it exists,
  * otherwise the original template.
  */
-function bmltenabled_mayo_loadDetailsTemplate($template)
+function Bmltenabled_Mayo_loadDetailsTemplate($template)
 {
     if (is_singular('mayo_event')) {
         $custom_template = plugin_dir_path(__FILE__) .
@@ -143,7 +143,7 @@ function bmltenabled_mayo_loadDetailsTemplate($template)
  *
  * @return void
  */
-function bmltenabled_mayo_enqueueAdminScripts()
+function Bmltenabled_Mayo_enqueueAdminScripts()
 {
     wp_enqueue_script(
         'admin-bundle',
@@ -153,4 +153,4 @@ function bmltenabled_mayo_enqueueAdminScripts()
         true
     );
 }
-add_action('enqueue_block_editor_assets', 'bmltenabled_mayo_enqueueAdminScripts');
+add_action('enqueue_block_editor_assets', 'Bmltenabled_Mayo_enqueueAdminScripts');
