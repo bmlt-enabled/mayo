e6d658661cdf812394d83e73de0a696179ecf046
Danny Gershman <danny.gershman@gmail.com>
2025-04-06 15:01:16 -0400

    css classes theme

--- assets/js/src/admin.js
+++ assets/js/src/admin.js
@@ -2,11 +2,13 @@
 import { render } from '@wordpress/element';
 import ShortcodesDocs from './components/admin/ShortcodesDocs';
 import Settings from './components/admin/Settings';
+import CssClassesDocs from './components/admin/CssClassesDocs';
 import './plugins/event-sidebar';
 
 document.addEventListener('DOMContentLoaded', () => {
     const shortcodesContainer = document.getElementById('mayo-shortcode-root');
     const settingsContainer = document.getElementById('mayo-settings-root');
+    const cssClassesContainer = document.getElementById('mayo-css-classes-root');
 
     if (shortcodesContainer) {
         render(<ShortcodesDocs />, shortcodesContainer);
@@ -15,4 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
     if (settingsContainer) {
         render(<Settings />, settingsContainer);
     }
-}); 
+
+    if (cssClassesContainer) {
+        render(<CssClassesDocs />, cssClassesContainer);
+    }
+})
