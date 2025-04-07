7338b369aab5bf96dba9046d78a70aeeb5d3066b
Danny Gershman <danny.gershman@gmail.com>
2025-04-06 20:46:44 -0400

    putting back single load

--- mayo-events-manager.php
+++ mayo-events-manager.php
@@ -22,10 +22,8 @@ if (! defined('ABSPATH') ) {
 
 define('MAYO_VERSION', '1.1.3');
 
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    require_once __DIR__ . '/vendor/autoload.php';
-}
-
+// Do not change this line.
+require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/includes/Admin.php';
 require_once __DIR__ . '/includes/Frontend.php';
 require_once __DIR__ . '/includes/Rest.php';
