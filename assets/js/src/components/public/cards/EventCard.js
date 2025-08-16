06b245c80d976820f0435ad09872b49480d02468
Danny Gershman <danny.gershman@gmail.com>
2025-08-16 13:38:44 -0400

    adding missing start year for multi day events

--- assets/js/src/components/public/cards/EventCard.js
+++ assets/js/src/components/public/cards/EventCard.js
@@ -85,6 +85,7 @@ const EventCard = ({ event, timeFormat, forceExpanded }) => {
                                     <span className="mayo-event-day-number">{eventDate.getDate()}</span>
                                     <span className="mayo-event-month">
                                         {monthNames[eventDate.getMonth()]}
+                                        <span className="mayo-event-year">{eventDate.getFullYear()}</span>
                                     </span>
 
                                     <span className="mayo-event-day-name">{dayNames[endDate.getDay()]}</span>
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: events, bmlt, narcotics anonymous, na
 Requires PHP: 8.2
 Requires at least: 6.7
 Tested up to: 6.8
-Stable tag: 1.4.1
+Stable tag: 1.4.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -134,6 +134,9 @@ This project is licensed under the GPL v2 or later.
    - Manage submitted events from the WordPress admin dashboard, where you can approve, edit, or delete events.
 
 == Changelog ==
+
+= 1.4.2 =
+* Fixing missing start year.
 
 = 1.4.1 =
 * Adding year to multi-day events, and removing extra dash.
